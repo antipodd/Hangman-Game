8829a74941a5467fce07a6076c01fdab15bacab4
Karel Hartlieb <Karel Hartlieb>
2017-02-10 16:38:22 -0600

    save021017

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
 var guesses = 12;
-var options = ["MARIO", "YOSHI", "LUIGI", "TOAD", "PEACH", "KOOPA", "GOOMBA", "LAKITU", "BOWSER", "TANOOKI", "DAISY", "PRINCESS", "MUSHROOM", "FLOWER"];
+var options = ["MARIO", "YOSHI", "LUIGI", "TOAD", "PEACH", "KOOPA", "GOOMBA", "LAKITU", "BOWSER", "TANOOKI", "DAISY", "PRINCESS", "MUSHROOM", "FIRE FLOWER","ICE FLOWER", "BULLET BILL", "SUPER STAR", "BLUE SHELL", "COIN"];
 var word = options[Math.floor(Math.random() * options.length)];
 var display = [];
 for (var i = 0; i < word.length; i++) {
+  if (word.charAt(i) === " ") {
+    display.push(" ");
+  } else {
   display.push("_");
+  }
 }
 var display2 = display.join(" ");
 console.log(display);
 console.log(display2);
 var guessedLetters = [];
+var wins = 0;
 
 function playSound1() {
   document.getElementById('coin').play();
@@ -21,6 +26,13 @@ function playWinningSound() {
 function playLosingSound() {
   document.getElementById('lose').play();
 }
+
+var myAudio = new Audio('../Hangman-Game/assets/sounds/SuperMarioBros.mp3'); 
+myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+myAudio.play();
 
 // This function is run whenever the user presses a key.
       document.onkeyup = function(event) {
@@ -35,13 +47,18 @@ function playLosingSound() {
         for(var j = 0; j < word.length; j++) {
           if (userGuess === word.charAt(j)) {
             display[j] = userGuess;
+            var count = 0; //ensure that sound for a repeated correct guess is not played again
+            for (var y = 0; y < guessedLetters.length; y++) {
+              if (userGuess === guessedLetters[y]) {
+                count++
+              }
+            }
+            if (count === 0) {
             playSound1();
-
-          } else {
-            //ensure that sound for a repeated correct guess is not played again
-          }
+            }
+          }          
         }
-        guesses--
+        
         var counter = 0;
         for (var guess = 0; guess < guessedLetters.length; guess++) {
           if (userGuess === guessedLetters[guess]) {
@@ -50,6 +67,7 @@ function playLosingSound() {
         }
         if (counter === 0) {
             guessedLetters.push(userGuess);
+            guesses--; //only guessed letters that have not been previously guessed cause a decrease in remaining guesses
         }
         console.log(guessedLetters);
         var display2 = display.join(" ");
@@ -60,12 +78,22 @@ function playLosingSound() {
         console.log(guessedLetters2);
         if (guesses === 0) {
           console.log("Game Over");
+          myAudio.pause();
           playLosingSound();
           //show losing image here
+          setTimeout(function() {
+          myAudio.currentTime = 0;
+          myAudio.play();
+          }, 3000);
+          
           word = options[Math.floor(Math.random() * options.length)];
           display = [];
           for (var i = 0; i < word.length; i++) {
+            if (word.charAt(i) === " ") {
+            display.push(" ");
+            } else {
             display.push("_");
+            }
           }
           var display2 = display.join(" ");
           console.log(display);
@@ -73,12 +101,24 @@ function playLosingSound() {
           guesses = 12;
           guessedLetters = [];
         } else if (display.join('') === word) {
+          myAudio.pause();
           playWinningSound();
+          //show winning image here
+          wins++;
+          setTimeout(function() {
+          myAudio.currentTime = 0;
+          myAudio.play();
+          }, 6000);
+          console.log(wins);
           //show image of character
           word = options[Math.floor(Math.random() * options.length)];
           display = [];
           for (var i = 0; i < word.length; i++) {
+            if (word.charAt(i) === " ") {
+            display.push(" ");
+            } else {
             display.push("_");
+            }
           }
           var display2 = display.join(" ");
           console.log(display);
