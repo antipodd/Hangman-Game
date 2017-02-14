55a806b8d0d2946ed7bf9a807760b0922b8c4892
Karel Hartlieb <Karel Hartlieb>
2017-02-13 18:21:59 -0600

    timeout error fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,9 +26,8 @@ function reset() {
   guessedLetters2 = guessedLetters.join(" ");
   document.querySelector('.guesses-no').innerHTML = guesses;
   document.querySelector('.display-letters').innerHTML = guessedLetters2;
-  //console.log(word);
+  //console.log(word);             
 }
-
 
 window.onload = function() { //need to run this after DOM is built otherwise display2 doesn't appear
   reset();
@@ -38,14 +37,13 @@ function playSound1() {
   var correct = document.getElementById('coin');
   if (correct.paused) {
         correct.play();
-    }else{ // allows coin to replay from beginning if time between guesses is shorter than length of smb_coin.wav
-        correct.pause();
-        correct.currentTime = 0;
-        setTimeout(function () { //Need to add this timeout function to prevent Promise Error     
-        correct.play();
-        }, 50);
-        
-    }
+  }else{ // allows coin to replay from beginning if time between guesses is shorter than length of smb_coin.wav
+    correct.pause();
+    correct.currentTime = 0;
+    setTimeout(function () { //Need to add this timeout function to prevent Promise Error     
+      correct.play();
+    }, 50);     
+  }
 }
 
 function playWinningSound() {
@@ -56,39 +54,39 @@ function playLosingSound() {
   document.getElementById('lose').play();
 }
 
-var myAudio = new Audio('../Hangman-Game/assets/sounds/SuperMarioBros.mp3'); 
+var myAudio = new Audio('./assets/sounds/SuperMarioBros.mp3'); 
 myAudio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 myAudio.play();
-
+var userGuess = "";
 
 // This function is run whenever the user presses a key.
-      document.onkeyup = function(event) {
-
-        if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {    
+document.onkeyup = function(event) {
+  if (/*myAudio.duration > 0 &&*/ !myAudio.paused) { // need this within document.onkeyup to prevent keyup events from doing anything during setTimeout
+    if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {    
        
 
       // Determines which key was pressed
-        var userGuess = event.key;
-        userGuess = userGuess.toUpperCase();
-        //console.log(userGuess);
-        var count = 0; //ensure that sound for a repeated correct guess is not played again and repeated corrected guesses are not added to guessedLetters array
-        var correct = 0;  //ensure that correct guess is not stored in guessed letters
-        for(var j = 0; j < word.length; j++) {
-          if (userGuess === display[j]) {
-            count++; //check for repeated correct guess
-          }
-          if (userGuess === word.charAt(j) && userGuess !== display[j]) {
-            display[j] = userGuess;
-            correct++;
-          }
+      userGuess = event.key;
+      userGuess = userGuess.toUpperCase();
+      //console.log(userGuess);
+      var count = 0; //ensure that sound for a repeated correct guess is not played again and repeated corrected guesses are not added to guessedLetters array
+      var correct = 0;  //ensure that correct guess is not stored in guessed letters
+      for(var j = 0; j < word.length; j++) {
+        if (userGuess === display[j]) {
+          count++; //check for repeated correct guess
         }
-        if (correct > 0 && count === 0) {
-          playSound1();
-          guesses--;
-          document.querySelector('.guesses-no').innerHTML = guesses;
+        if (userGuess === word.charAt(j) && userGuess !== display[j]) {
+          display[j] = userGuess;
+          correct++;
+        }
+      }
+      if (correct > 0 && count === 0) {
+        playSound1();
+        guesses--;
+        document.querySelector('.guesses-no').innerHTML = guesses;
 
             /*for (var y = 0; y < display.length; y++) {
               if (userGuess === display[y]) {
@@ -101,101 +99,117 @@ myAudio.play();
             if (correct > 0) {
               guesses--;
             }*/
-          }          
+      }          
         
         
-        var counter = 0;
-        for (var guess = 0; guess < guessedLetters.length; guess++) {
-          if (userGuess === guessedLetters[guess]) {
-            counter++;
-          } //adding guessed letter into an array and checking the array to make sure the guess isn't repeated in the array
-        }
-        if (counter === 0 && correct === 0 && count === 0) {
-            guessedLetters.push(userGuess);
-            guesses--; //only incorrect guessed letters that have not been previously guessed cause a decrease in remaining guesses
-            document.querySelector('.guesses-no').innerHTML = guesses;
-        } /*else {
+      var counter = 0;
+      for (var guess = 0; guess < guessedLetters.length; guess++) {
+        if (userGuess === guessedLetters[guess]) {
+          counter++;
+        } //adding guessed letter into an array and checking the array to make sure the guess isn't repeated in the array
+      }
+      if (counter === 0 && correct === 0 && count === 0) {
+        guessedLetters.push(userGuess);
+        guesses--; //only incorrect guessed letters that have not been previously guessed cause a decrease in remaining guesses
+        document.querySelector('.guesses-no').innerHTML = guesses;
+      } /*else {
           guesses--;
         }*/
         //console.log(guessedLetters);
-        var display2 = display.join("");
-        document.querySelector('.word-display').innerHTML = display2;
+      var display2 = display.join("");
+      document.querySelector('.word-display').innerHTML = display2;
         //console.log(display);
         //console.log(display2);
         //console.log(guesses);
-        var guessedLetters2 = guessedLetters.join(" ");
+      var guessedLetters2 = guessedLetters.join(" ");
         //console.log(guessedLetters2);
-        document.querySelector('.display-letters').innerHTML = guessedLetters2;
-        if (display.join('') === word) {
-          myAudio.pause();
-          playWinningSound();
+      document.querySelector('.display-letters').innerHTML = guessedLetters2;
+      if (display.join('') === word) {
+        myAudio.pause();
+        playWinningSound();
+          //console.log(document.getElementById('win').play());
           //show winning image here
-            if (word === "MARIO") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/180px-Mario_Paper.jpg">' + '<p>Mario</p>';
-            } else if (word === "YOSHI") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/yoshi.jpg">' + '<p>Yoshi</p>';
-            } else if (word === "LUIGI") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Luigi0.png">' + '<p>Luigi</p>';
-            } else if (word === "TOAD") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Red_Blue_Toad.png">' + '<p>Toad</p>';
-            } else if (word === "PEACH") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Peach.png">' + '<p>Peach</p>';
-            } else if (word === "KOOPA") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Koopa.jpg">' + '<p>Koopa</p>';
-            } else if (word === "GOOMBA") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Goomba.jpg">' + '<p>Goomba</p>';
-            } else if (word === "LAKITU") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/lakitu.jpg">' + '<p>Lakitu</p>';
-            } else if (word === "BOWSER") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Bowser_-_New_Super_Mario_Bros_2.png">' + '<p>Bowser</p>';
-            } else if (word === "TANOOKI") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/180px-TanookiMario_SMB3.jpg">' + '<p>Tanooki</p>';
-            } else if (word === "DAISY") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Daisy.jpg">' + '<p>Daisy</p>';
-            } else if (word === "PRINCESS") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/princess.jpg">' + '<p>Princess</p>';
-            } else if (word === "MUSHROOM") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/200px-MushroomMarioKart8.png">' + '<p>Mushroom</p>';
-            } else if (word === "FIRE FLOWER") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/200px-FireFlowerMK8.png">' + '<p>Fire Flower</p>';
-            } else if (word === "ICE FLOWER") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/ice-flower.png">' + '<p>Ice Flower</p>';
-            } else if (word === "BULLET BILL") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/nsmb-bullet-bill.jpg">' + '<p>Bullet Bill</p>';
-            } else if (word === "SUPER STAR") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/1953218.jpg">' + '<p>Super Star</p>';
-            } else if (word === "BLUE SHELL") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Blue_Shell.png">' + '<p>Blue Shell</p>';
-            } else if (word === "COIN") {
-              document.querySelector('.image-area').innerHTML = '<img src="assets/images/Retro-Coin-icon.png">' + '<p>Coin</p>';
-            } 
-          wins++;
-          document.querySelector('.win-no').innerHTML = wins;
-          setTimeout(function() {
-          myAudio.currentTime = 0;
-          myAudio.play();
-          /*document.body.onkeyup = null;*/
-          reset();
-          }, 5500); //start new game at end of winning sound
-          //console.log(wins);
-        } else if (guesses === 0 && display.join('') !== word) { //prevent game over appearing if word correctly guessed on last guess
-          //console.log("Game Over");
-          myAudio.pause();
-          playLosingSound();
-          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Bowser_emblem.jpg">' + '<p>Game Over</p>'; 
-          setTimeout(function() {
-          myAudio.currentTime = 0;
-          myAudio.play();
-          reset();
-          }, 3000); //start new game at end of losing sound
-          
-          
+        if (word === "MARIO") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/180px-Mario_Paper.jpg">' + '<p>Mario</p>';
+        } else if (word === "YOSHI") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/yoshi.jpg">' + '<p>Yoshi</p>';
+        } else if (word === "LUIGI") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Luigi0.png">' + '<p>Luigi</p>';
+        } else if (word === "TOAD") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Red_Blue_Toad.png">' + '<p>Toad</p>';
+        } else if (word === "PEACH") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Peach.png">' + '<p>Peach</p>';
+        } else if (word === "KOOPA") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Koopa.jpg">' + '<p>Koopa</p>';
+        } else if (word === "GOOMBA") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Goomba.jpg">' + '<p>Goomba</p>';
+        } else if (word === "LAKITU") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/lakitu.jpg">' + '<p>Lakitu</p>';
+        } else if (word === "BOWSER") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Bowser_-_New_Super_Mario_Bros_2.png">' + '<p>Bowser</p>';
+        } else if (word === "TANOOKI") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/180px-TanookiMario_SMB3.jpg">' + '<p>Tanooki</p>';
+        } else if (word === "DAISY") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Daisy.jpg">' + '<p>Daisy</p>';
+        } else if (word === "PRINCESS") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/princess.jpg">' + '<p>Princess</p>';
+        } else if (word === "MUSHROOM") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/200px-MushroomMarioKart8.png">' + '<p>Mushroom</p>';
+        } else if (word === "FIRE FLOWER") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/200px-FireFlowerMK8.png">' + '<p>Fire Flower</p>';
+        } else if (word === "ICE FLOWER") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/ice-flower.png">' + '<p>Ice Flower</p>';
+        } else if (word === "BULLET BILL") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/nsmb-bullet-bill.jpg">' + '<p>Bullet Bill</p>';
+        } else if (word === "SUPER STAR") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/1953218.jpg">' + '<p>Super Star</p>';
+        } else if (word === "BLUE SHELL") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Blue_Shell.png">' + '<p>Blue Shell</p>';
+        } else if (word === "COIN") {
+          document.querySelector('.image-area').innerHTML = '<img src="assets/images/Retro-Coin-icon.png">' + '<p>Coin</p>';
         } 
-        } else {
-          alert("Type a letter between a and z")
-          }
-        }
-        
+        wins++;
+        document.querySelector('.win-no').innerHTML = wins;
+          
+          /*var b = document.querySelector('.win-no');
+          window.addEventListener('keyup', function (event) {
+            b.setAttribute("disabled", "disabled");
+          
+          
+
+        // prevent default behaviour
+          
+    })*/
+
+
+          /*var b = document.querySelector('body');
+          document.onkeyup = function() {
+            
+            b.setAttribute("disabled", "disabled");
+          }*/
+        setTimeout(function() {
+          myAudio.currentTime = 0;
+          myAudio.play();
+          reset();
+        }, 5500); //start new game at end of winning sound
+          //console.log(wins);
+      } else if (guesses === 0 && display.join('') !== word) { //prevent game over appearing if word correctly guessed on last guess
+          //console.log("Game Over");
+        myAudio.pause();
+        playLosingSound();
+        document.querySelector('.image-area').innerHTML = '<img src="assets/images/Bowser_emblem.jpg">' + '<p>Game Over</p>'; 
+        setTimeout(function() {
+          myAudio.currentTime = 0;
+          myAudio.play();
+          reset();
+        }, 3000); //start new game at end of losing sound
+      }
+          
+    } else {
+      alert("Type a letter between a and z")
+    }
+  }
+} 
       
 
-        
+
